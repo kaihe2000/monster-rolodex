f76b5d08a33c9aae5ca49dd35b5185da08642ca1
Kai He <KaiHe@Kais-MacBook-Pro.local>
2019-12-16 22:26:28 -0800

    Updates

---
+++ precache-manifest.a5a8af4a1ef10c92a4521029ada09d4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118a24bef8752be7dc67c9dba9ffcf3c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "0b83c2f802ed16d0319a",
+    "url": "/monster-rolodex/static/css/main.af2c3fab.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b83c2f802ed16d0319a",
+    "url": "/monster-rolodex/static/js/main.f92bbdd3.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
